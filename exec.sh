9e82cd0d1276d2984e8fe3831a052d26cea57d60
root <root@rock.localdomain>
2018-06-14 10:20:08 +0000

    update

--- exec.sh
+++ exec.sh
@@ -5,13 +5,15 @@ tag=$TAG
 ts=$(date +%s)
 
 cd repo
-git fetch origin $BRANCH:refs/remotes/origin/$BRANCH
-git checkout-index -a -f --prefix=../refs/$ts/
-echo "built: $ts $branch $tag"
+git fetch origin $branch:refs/remotes/origin/$branch
+mkdir ../refs/$ts
+git --work-tree=$(pwd)/../refs/$ts checkout origin/$branch -- .
+echo "fetched: $ts $branch"
 
 cd ../refs/$ts
 docker pull $tag # check update before build
 docker build -t $tag .
+echo "built: $ts $branch $tag"
 
 cd ../..
 rm -rf refs/$ts
