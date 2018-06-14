c1ccf92b78f51cfb1f1b08d8e7fd7294c3fbeaa0
Michael JIN <emj365@gmail.com>
2018-06-14 17:47:38 +0800

    update

--- exec.sh
+++ exec.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+branch=$BRANCH
+tag=$TAG
 ts=$(date +%s)
 
 cd repo
-git fetch origin staging:refs/remotes/origin/staging
+git fetch origin $BRANCH:refs/remotes/origin/$BRANCH
 git checkout-index -a -f --prefix=../refs/$ts/
+echo "built: $ts $branch $tag"
+
 cd ../refs/$ts
-docker build -t emj365/prepsmith:latest .
-docker push emj365/prepsmith:latest
+docker build -t $tag .
 
 cd ../..
-echo $ts >> log
+rm -rf refs/$ts
+
+docker push $tag
+echo "pushed: $ts $branch $tag"
--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-var express = require('express');
+// Example:
+// SECERT=abc123 REPO_URL='https://github.com/emj365/prepsmith' BRANCH='staging' yarn start
+
+var express = require('express')
 var bodyParser = require('body-parser')
-var shell = require('shelljs');
-var crypto = require('crypto');
+var shell = require('shelljs')
+var crypto = require('crypto')
 
-var app = express();
+var app = express()
 
-var SECERT=process.env['SECERT']
-var REPO_URL=process.env['REPO_URL']
-var REF=process.env['REF']
+var SECERT = process.env['SECERT']
+var REPO_URL = process.env['REPO_URL']
+var BRANCH = process.env['BRANCH']
+var TAG = process.env['TAG']
+var REF = 'refs/heads/' + BRANCH
+
+// validate a X-Hub-Signature header
+// https://gist.github.com/kiewic/a419b8e47b3baf9a301dee598d6ade87
 
 // Calculate the X-Hub-Signature header value.
 function getSignature(buf) {
-  var hmac = crypto.createHmac("sha1", SECERT);
-  hmac.update(buf, "utf-8");
-  return "sha1=" + hmac.digest("hex");
+  var hmac = crypto.createHmac('sha1', SECERT)
+  hmac.update(buf, 'utf-8')
+  return 'sha1=' + hmac.digest('hex')
 }
 
 // Verify function compatible with body-parser to retrieve the request payload.
 // Read more: https://github.com/expressjs/body-parser#verify
 function verifyRequest(req, res, buf, encoding) {
-  var expected = req.headers['x-hub-signature'];
-  var calculated = getSignature(buf);
-  console.log("X-Hub-Signature:", expected, "Content:", "-" + buf.toString('utf8') + "-");
+  var expected = req.headers['x-hub-signature']
+  var calculated = getSignature(buf)
+  // console.log(
+  //   'X-Hub-Signature:',
+  //   expected,
+  //   'Content:',
+  //   '-' + buf.toString('utf8') + '-'
+  // )
   if (expected !== calculated) {
-    throw new Error("Invalid signature.");
+    throw new Error('Invalid signature.')
   } else {
-    console.log("Valid signature!");
+    console.log('Valid signature!')
   }
 }
 
@@ -33,29 +46,29 @@ function verifyRequest(req, res, buf, encoding) {
 // Read more: http://expressjs.com/en/guide/error-handling.html
 function abortOnError(err, req, res, next) {
   if (err) {
-    console.log(err);
-    res.status(400).send({ error: "Invalid signature." });
+    console.log(err)
+    res.status(400).send({ error: 'Invalid signature.' })
   } else {
-    next();
+    next()
   }
 }
+// ENDOF validate a X-Hub-Signature header
 
-app.use(bodyParser.json({ verify: verifyRequest }));
+app.use(bodyParser.json({ verify: verifyRequest }))
 
-app.use(abortOnError);
+app.use(abortOnError)
 
-app.post('/ping', function (req, res) {
-  res.send('ok');
-});
+app.post('/ping', function(req, res) {
+  res.sendStatus(200)
+})
 
-app.post('/', function (req, res) {
-  res.send('ok');
+app.post('/', function(req, res) {
+  res.sendStatus(200)
   if (req.body.repository.url === REPO_URL && req.body.ref === REF) {
-    shell.exec('./exec.sh')
+    shell.exec(`BRANCH=${BRANCH} TAG=${TAG} ./exec.sh`)
   }
-});
+})
 
-app.listen(80, function () {
-  console.log('app listening on port 80!');
-});
-
+app.listen(80, function() {
+  console.log('app listening on port 80!')
+})
